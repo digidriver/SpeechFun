4a8237a981ee7921a719a207750bd960717912d2
digidriver <slippers99@hotmail.com>
2017-06-14 23:45:14 -0400

    First commit

---
+++ SpeechFun/FormMain.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SpeechFun
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSpeak = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxPhrase = new System.Windows.Forms.TextBox();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.labelTextBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonSpeak
+            // 
+            this.buttonSpeak.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSpeak.Location = new System.Drawing.Point(12, 349);
+            this.buttonSpeak.Name = "buttonSpeak";
+            this.buttonSpeak.Size = new System.Drawing.Size(150, 50);
+            this.buttonSpeak.TabIndex = 0;
+            this.buttonSpeak.Text = "Speak!";
+            this.buttonSpeak.UseVisualStyleBackColor = true;
+            this.buttonSpeak.Click += new System.EventHandler(this.buttonSpeak_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(272, 349);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(150, 50);
+            this.buttonExit.TabIndex = 1;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxPhrase
+            // 
+            this.textBoxPhrase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPhrase.Location = new System.Drawing.Point(12, 184);
+            this.textBoxPhrase.Name = "textBoxPhrase";
+            this.textBoxPhrase.Size = new System.Drawing.Size(410, 26);
+            this.textBoxPhrase.TabIndex = 2;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(12, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(343, 37);
+            this.labelTitle.TabIndex = 3;
+            this.labelTitle.Text = "Speak! (Speech demo)";
+            // 
+            // labelTextBox
+            // 
+            this.labelTextBox.AutoSize = true;
+            this.labelTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextBox.Location = new System.Drawing.Point(12, 161);
+            this.labelTextBox.Name = "labelTextBox";
+            this.labelTextBox.Size = new System.Drawing.Size(175, 20);
+            this.labelTextBox.TabIndex = 4;
+            this.labelTextBox.Text = "Enter your phrase here:";
+            // 
+            // FormMain
+            // 
+            this.AcceptButton = this.buttonSpeak;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 411);
+            this.Controls.Add(this.labelTextBox);
+            this.Controls.Add(this.labelTitle);
+            this.Controls.Add(this.textBoxPhrase);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonSpeak);
+            this.Name = "FormMain";
+            this.Text = "Speak!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSpeak;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxPhrase;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Label labelTextBox;
+    }
+}
+
---
+++ SpeechFun/FormMain.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace SpeechFun
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonSpeak_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer synthVoice = new SpeechSynthesizer();
+            synthVoice.Speak(textBoxPhrase.Text);            
+        }
+    }
+}
